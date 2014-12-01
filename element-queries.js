dc06b2601bb289cee0bcefa2a059d6a7793ee7d2
Mark Funk <mark@boomtownroi.com>
2014-12-01 14:56:39 -0500

    Updating public api with add and remove methods

--- element-queries.js
+++ element-queries.js
@@ -1,7 +1,8 @@
 (function(window, undefined) {
   'use strict';
 
-  var elementQueryComponents = [];
+  var elementQueryComponents = {};
+  var elementKey = 0;
 
   function debounce(func, wait, immediate) {
     var timeout;
@@ -18,8 +19,25 @@
     };
   }
 
+  function add(nameOrObject, descriptor) {
+    var key;
+    if (typeof nameOrObject === 'string') {
+      key = nameOrObject;
+      elementQueryComponents[nameOrObject] = descriptor;
+    } else {
+      key = elementKey++;
+      elementQueryComponents[key] = nameOrObject;
+    }
+    return key;
+  }
+
+  function remove(key) {
+    delete elementQueryComponents[key];
+  }
+
   function calculateElements() {
-    var componentsCounter = elementQueryComponents.length;
+    var componentKeys = Object.keys(elementQueryComponents);
+    
     var currentComponent;
     var $elements;
 
@@ -42,22 +60,23 @@
       }
     }
 
-    while (componentsCounter--) {
-      currentComponent = elementQueryComponents[componentsCounter];
+    componentKeys.forEach(function(key) {
+      currentComponent = elementQueryComponents[key];
       if (currentComponent.name) {
         $elements = document.querySelectorAll(currentComponent.name);
       } else if (currentComponent.element) {
         $elements = [currentComponent.element];
       }
       Array.prototype.forEach.call($elements, calculateElement);
-    }
+    });
   }
 
   window.addEventListener("load",   calculateElements);
   window.addEventListener("resize", debounce(calculateElements, 100), false);
 
   window.btElementQueries = {
-    components: elementQueryComponents,
+    add:       add,
+    remove:    remove,
     calculate: calculateElements
   };
 })(window);
