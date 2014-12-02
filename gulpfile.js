98195bb8636e7ac6a95f686037ff0e51de28edbd
Craig Anthony <mcraiganthony@gmail.com>
2014-12-02 11:15:05 -0500

    Craig - index page, styles, browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,24 @@ var pkg          = require('./package.json'),
     jshint       = require("gulp-jshint"),
     merge        = require("merge-stream"),
     header       = require("gulp-header"),
-    livereload   = require("gulp-livereload");
+    autoprefixer = require("gulp-autoprefixer"),
+    ghpages      = require("gulp-gh-pages"),
+    less         = require('gulp-less'),
+    browserSync  = require('browser-sync'),
+    reload       = browserSync.reload;
 
 // Used for copyright headers
 var banner = "/*! <%= pkg.title %> <%= pkg.version %> | <%= pkg.homepage %> | (c) 2014 BoomTown | MIT License */\n";
+
+
+// Styles
+gulp.task('styles', function() {
+  return gulp.src(["src/less/app.less"])
+    .pipe(less({ compress: true }))
+    .pipe(autoprefixer({ browsers: ['last 2 versions','ie 9'], cascade: false }))
+    .pipe(gulp.dest("dist/css/"))
+    .pipe(reload({stream:true}));
+});
 
 // JS Hint
 gulp.task("jshint", function() {
@@ -30,34 +44,51 @@ gulp.task("jshint", function() {
 
 // Scripts
 gulp.task("scripts", function() {
-  var uncompressed = gulp.src("src/*.js")
+  var uncompressed = gulp.src("src/js/*.js")
     .pipe(header(banner, { pkg : pkg }))
-    .pipe(gulp.dest("dist/js/"));
+    .pipe(gulp.dest("dist/js/"))
+    .pipe(reload({stream:true}));
 
-  var compressed = gulp.src("src/*.js")
+  var compressed = gulp.src("src/js/*.js")
     .pipe(uglify())
     .pipe(header(banner, { pkg : pkg }))
     .pipe(rename({
       suffix: ".min"
     }))
-    .pipe(gulp.dest("dist/js/"));
+    .pipe(gulp.dest("dist/js/"))
+    .pipe(reload({stream:true}));
 
   return merge(uncompressed, compressed);
 });
 
-// Default task
-gulp.task("default", ["jshint", "scripts"]);
-
 // Watch
 gulp.task("watch", function () {
-  gulp.watch("src/*.js", ["jshint", "scripts"]);
+  gulp.watch("src/less/*.less", ["styles"]);
+  gulp.watch("src/js/*.js", ["jshint", "scripts"]);
+});
 
-  // Create LiveReload server
-  var server = livereload();
-  livereload.listen();
-
-  // Watch files in patterns below, reload on change
-  gulp.watch("dist/js/*.js").on("change", function(file) {
-    server.changed(file.path);
+// Browser Sync
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: 'dist/'
+    }
   });
 });
+
+// Website
+gulp.task('website', function () {
+  return gulp.src('./dist/**/*')
+    .pipe(ghpages());
+});
+
+// Server
+gulp.task('server', ['watch', 'browser-sync'], function () {
+    gulp.watch("dist/*.html", reload);
+});
+
+// Default task
+gulp.task("default", ["styles", "jshint", "scripts"]);
+
+
+
