6eda8382c62d3e6f7eef9679c0b29c0cf58a8730
Mark Funk <mark@boomtownroi.com>
2015-01-02 11:02:05 -0500

    updating package json and gulpfile to remove tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,36 @@
+'use strict';
 // Load Plugins
 var pkg          = require('./package.json'),
-    gulp         = require("gulp"),
-    uglify       = require("gulp-uglify"),
-    rename       = require("gulp-rename"),
-    jshint       = require("gulp-jshint"),
-    merge        = require("merge-stream"),
-    header       = require("gulp-header");
+    gulp         = require('gulp'),
+    jshint       = require('gulp-jshint'),
+    header       = require('gulp-header');
 
 // Used for copyright headers
-var banner = "/*! <%= pkg.title %> <%= pkg.version %> | <%= pkg.homepage %> | (c) 2014 BoomTown | MIT License */\n";
+var banner = '/*! <%= pkg.title %> <%= pkg.version %> | <%= pkg.homepage %> | (c) 2014 BoomTown | MIT License */\n';
 
 // JS Hint
-gulp.task("jshint", function() {
-  return gulp.src("src/*.js")
+gulp.task('jshint', function() {
+  return gulp.src('src/*.js')
     .pipe(jshint({
-      "boss": true,
-      "sub": true,
-      "evil": true,
-      "browser": true,
-      "multistr": true,
-      "globals": {
-        "module": false,
-        "require": true
+      'boss': true,
+      'sub': true,
+      'evil': true,
+      'browser': true,
+      'multistr': true,
+      'globals': {
+        'module': false,
+        'require': true
       }
     }))
-    .pipe(jshint.reporter("jshint-stylish"));
+    .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // Scripts
-gulp.task("scripts", function() {
-  var uncompressed = gulp.src("src/js/*.js")
+gulp.task('scripts', function() {
+  return gulp.src('src/js/*.js')
     .pipe(header(banner, { pkg : pkg }))
-    .pipe(gulp.dest("dist/js/"));
-
-  var compressed = gulp.src("src/js/*.js")
-    .pipe(uglify())
-    .pipe(header(banner, { pkg : pkg }))
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(gulp.dest("dist/js/"));
-
-  return merge(uncompressed, compressed);
-});
-
-// Watch
-gulp.task("watch", function () {
-  gulp.watch("src/js/*.js", ["jshint", "scripts"]);
+    .pipe(gulp.dest('dist/js/'));
 });
 
 // Default task
-gulp.task("default", ["jshint", "scripts"]);
+gulp.task('default', ['jshint', 'scripts']);
